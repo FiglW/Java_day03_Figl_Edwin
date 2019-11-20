5393312a1218c7361a1b6dba78119f82c05053ae
Wolfgang Figl <wolfgang.figl@me.com>
2019-11-20 10:52:13 +0100

    meins

---
+++ src/D3A1_W.java
@@ -0,0 +1,21 @@
+public class D3A1_W {
+
+    public static void main(String[] args) {
+
+        String hannah = "Did Hannah see bees? Hannah did.";
+
+        System.out.println(hannah.length());
+
+        System.out.println(hannah.charAt(12));
+
+        int letter = hannah.indexOf("b");
+
+        System.out.println(letter);
+
+
+        System.out.println("Was it a car or a cat I saw?".substring(9, 12));
+
+        
+
+    }
+}
